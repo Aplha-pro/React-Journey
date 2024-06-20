adb87d2d8cf20797248d812581e18c1bf615b232
Muhammad Ahmed <96483397+Aplha-pro@users.noreply.github.com>
2024-06-20 22:32:08 +0500

    Update Heading.jsx

--- Fragmentation/src/components/Heading.jsx
+++ Fragmentation/src/components/Heading.jsx
@@ -1,13 +1,39 @@
+// function Header() {
+//   let foodItem = ["Dal", "Green Vegetabels", "Beans"];
+//   if (foodItem.length < 5) {
+//     return <h3>Items are less than 5</h3>;
+//   } else {
+//     return (
+//       <>
+//         <h1>Healthy Food</h1>
+//         <ul className="list-group">
+//           {foodItem.map((item) => (
+//             <li key={item} className="list-group-item">
+//               {item}
+//             </li>
+//           ))}
+//         </ul>
+//       </>
+//     );
+//   }
+// }
+// export default Header;
 function Header() {
-    let foodItem = ['Dal' , 'Green Vegetabels' , 'Beans']
-    let foodId = [1,2,3]
+  let foodItem = [];
+
   return (
     <>
       <h1>Healthy Food</h1>
+      {foodItem.length === 0 ? <h3>Items are less than 5</h3> : null}
       <ul className="list-group">
-        {foodItem.map((item) => (<li key={item} className="list-group-item">{item}</li>))}
+        {foodItem.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
+
 export default Header;
