b920206bc669d46833838ba59cd43d729ac902ed
Muhammad Ahmed <96483397+Aplha-pro@users.noreply.github.com>
2024-06-20 20:35:42 +0500

    Update Heading.jsx

--- Fragmentation/src/components/Heading.jsx
+++ Fragmentation/src/components/Heading.jsx
@@ -1,4 +1,12 @@
-function Header(){
-    return <h1>Fragmentation</h1>
+function Header() {
+  return (
+    <ul class="list-group">
+      <li class="list-group-item">An item</li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+      <li class="list-group-item">A fourth item</li>
+      <li class="list-group-item">And a fifth one</li>
+    </ul>
+  );
 }
 export default Header;
