d7cdf4298153a49bff7cbb5b4cebb640d86098b8
Muhammad Ahmed <96483397+Aplha-pro@users.noreply.github.com>
2024-06-20 22:20:59 +0500

    Update Heading.jsx

--- Fragmentation/src/components/Heading.jsx
+++ Fragmentation/src/components/Heading.jsx
@@ -1,10 +1,11 @@
 function Header() {
     let foodItem = ['Dal' , 'Green Vegetabels' , 'Beans']
+    let foodId = [1,2,3]
   return (
     <>
       <h1>Healthy Food</h1>
-      <ul class="list-group">
-        {foodItem.map((item) => (<li class="list-group-item">{item}</li>))}
+      <ul className="list-group">
+        {foodItem.map((item) => (<li key={item} className="list-group-item">{item}</li>))}
       </ul>
     </>
   );
